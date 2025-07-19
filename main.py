7ef6159d345b3e69e323c6c337bd19b1f77a3abb
saadisaadibot <alhossiniabdulhalim3@gmail.com>
2025-07-19 11:04:49 +0200

    Update main.py
    اا

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ BOT_TOKEN = "8009488976:AAGU5x04wCdDavSoxzEM77SF17ZB_6QP-wU"
 @app.route(f"/webhook/{BOT_TOKEN}", methods=["POST"])
 def webhook():
     data = request.get_json()
+    
     if "message" in data:
         chat = data["message"]["chat"]
         chat_id = chat.get("id")
         username = chat.get("username", "بدون اسم مستخدم")
         print(f"✅ Chat ID: {chat_id} | Username: {username}")
+        print(data)  # <-- أضف هذا السطر
     return "ok"
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
